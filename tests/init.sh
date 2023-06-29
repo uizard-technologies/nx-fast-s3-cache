983fb1d8d07f0c757a6b263331899259144af0f0
Alexandros Solanos <alex@uizard.io>
2023-06-29 12:04:48 +0200

    Integration testing (#1)

---
+++ tests/init.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+if [[ "${ACT:-false}" == "true" ]]; then
+  # install missing packages if running using nektos/act
+  sudo apt-get update
+  sudo apt-get install -y parallel pigz
+fi
+
+# Install localstack and dependencies of sample repo
+parallel ::: 'pip install localstack awscli-local[ver1]' 'docker pull localstack/localstack' 'cd repo && npm install --no-audit --no-fund'
+
+localstack start -d
+
+echo "Waiting for LocalStack startup..."
+localstack wait -t 30
+echo "Startup complete"
+
+awslocal s3 mb s3://cache-bucket
---
+++ tests/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+cd "$(dirname "$0")"
+
+cd repo
+
+npx --no nx run sample-project:init-ci
+npx --no nx run sample-project:build-ci
+
+awslocal s3 sync s3://cache-bucket/ cache-bucket
+
+# make sure that cache-bucket contains exactly 2 .tar.gz files:
+
+find cache-bucket -type f -name '*.tar.gz' | wc -l | grep -q '^2$'
